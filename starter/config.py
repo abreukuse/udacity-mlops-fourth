c19e8a334baf8eee2954ddd958e3675f0bcaa2d1
abreukuse <tobias.abreu.kuse@gmail.com>
2024-02-03 13:36:23 -0300

    Set model path

--- starter/config.py
+++ starter/config.py
@@ -4,6 +4,10 @@ DATA_DIR = "data"
 DATA_FILE = "census.csv"
 DATA_PATH = os.path.join(DATA_DIR, DATA_FILE)
 
+MODEL_DIR = "model"
+MODEL_FILE = "model.pkl"
+MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
+
 HYPERPARAMETERS = {
     'max_depth': 10,
     'min_samples_split': 5,
