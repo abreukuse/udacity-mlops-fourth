49fe86f4dcb441268a800270febbd6f2f7f5edf1
abreukuse <tobias.abreu.kuse@gmail.com>
2024-02-03 13:16:18 -0300

    Add hyperparameters for a decision tree

--- starter/config.py
+++ starter/config.py
@@ -3,3 +3,12 @@ import os
 DATA_DIR = "data"
 DATA_FILE = "census.csv"
 DATA_PATH = os.path.join(DATA_DIR, DATA_FILE)
+
+HYPERPARAMETERS = {
+    'max_depth': 10,
+    'min_samples_split': 5,
+    'min_samples_leaf': 2,
+    'max_features': 'sqrt',
+    'criterion': 'gini',
+    'class_weight': 'balanced'
+}
