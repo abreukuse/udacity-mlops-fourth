55bffe32c6f570157dbf1a5611cdf5f274a0b09d
abreukuse <tobias.abreu.kuse@gmail.com>
2024-02-04 14:14:59 -0300

    Check if the categorical variiable if a valid one.

--- starter/data_slice_model_performance.py
+++ starter/data_slice_model_performance.py
@@ -4,6 +4,7 @@ Perform model validation on slices of the data.
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from textwrap import dedent
 from ml.data import process_data
 from ml.model import (
 	compute_model_metrics,
@@ -21,54 +22,59 @@ from starter.config import (
 
 
 def data_slice_validation():
-	data = pd.read_csv(DATA_PATH)
-	model, encoder, binarizer = load_model()
+	if FEATURE_SLICE in CATEGORICAL_FEATURES:
+		data = pd.read_csv(DATA_PATH)
+		model, encoder, binarizer = load_model()
 
-	train, test = train_test_split(
-	    data, 
-	    test_size=0.20,
-	    random_state=42
-	)
+		train, test = train_test_split(
+		    data, 
+		    test_size=0.20,
+		    random_state=42
+		)
 
-	feature_unique_values = test[FEATURE_SLICE].unique()
+		feature_unique_values = test[FEATURE_SLICE].unique()
 
-	with open("slice_output.txt", 'w') as f:
-		print_variable = f"\tVariable: {FEATURE_SLICE}"
-		print(print_variable, file=f)
-		print(print_variable)
-		
-		for value in feature_unique_values:
-			data_slice = test.loc[test[FEATURE_SLICE]==value, :]
+		with open("slice_output.txt", 'w') as f:
+			print_variable = f"\tVariable: {FEATURE_SLICE}"
+			print(print_variable, file=f)
+			print(print_variable)
 
-			feature_matrix, ground_truth, _, _ = process_data(
-				X=data_slice,
-				categorical_features=CATEGORICAL_FEATURES,
-				label=TARGET_VARIABLE,
-				training=False,
-				encoder=encoder,
-				lb=binarizer
-			)
+			for value in feature_unique_values:
+				data_slice = test.loc[test[FEATURE_SLICE]==value, :]
 
-			model_predictions = inference(
-				model=model,
-				X=feature_matrix
-			)
+				feature_matrix, ground_truth, _, _ = process_data(
+					X=data_slice,
+					categorical_features=CATEGORICAL_FEATURES,
+					label=TARGET_VARIABLE,
+					training=False,
+					encoder=encoder,
+					lb=binarizer
+				)
 
-			model_performance = compute_model_metrics(
-				y=ground_truth,
-				preds=model_predictions
-			)
+				model_predictions = inference(
+					model=model,
+					X=feature_matrix
+				)
 
-			precision, recall, fbeta = model_performance
+				model_performance = compute_model_metrics(
+					y=ground_truth,
+					preds=model_predictions
+				)
 
-			print_results = f"""
-			Value: {value}
-				- Precision: {precision}
-				- Recall: {recall}
-				- Fbeta: {fbeta}
-			"""
-			print(print_results, file=f)
-			print(print_results)
+				precision, recall, fbeta = model_performance
+
+				print_results = dedent(f"""
+				Value: {value}
+					- Precision: {precision}
+					- Recall: {recall}
+					- Fbeta: {fbeta}
+				""")
+				print(print_results, file=f)
+				print(print_results)
+	else:
+		print(
+			"Please specify a valid categorical feature in the 'FEATURE_SLICE' variable within the 'config.py' file."
+		)
 
 
 if __name__ == '__main__':
