1aece77cc7dbbc6ebac74236cb09e4962fe77afa
abreukuse <tobias.abreu.kuse@gmail.com>
2024-02-08 18:45:26 -0300

    Code style flake8

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 def test_api_root():
     response = client.get("/")
-    
+
     assert response.status_code == 200
     assert response.json() == {"ML Model": "Welcome!"}
 
